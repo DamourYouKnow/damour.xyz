53b1834a4264c4250cbc42dc4aed64b1bb7b4766
damouryouknow <bailey@damour.xyz>
2019-08-27 21:16:06 -0400

    Add deployment target to known hosts

--- .github/deploy/entrypoint.sh
+++ .github/deploy/entrypoint.sh
@@ -6,13 +6,19 @@ bundle list | grep "jekyll ("
 echo 'Building site...'
 bundle exec jekyll build
 
-touch sshkey
-echo $SSH_KEY > sshkey
+mkdir -p /root/.ssh && \
+    chmod 0700 /root/.ssh && \
+    ssh-keyscan damour.xyz > /root/.ssh/known_hosts && \
+    # Add the keys and set permissions
+    echo "$SSH_KEY" > /root/.ssh/id_rsa && \
+    chmod 600 /root/.ssh/id_rsa
 
 echo 'Deleting old site...'
-ssh github@damour.xyz "rm -rf /var/www/damour.xyz/*" -i sshkey -o "StrictHostKeyChecking=no"
+ssh github@damour.xyz "rm -rf /var/www/damour.xyz/*"
 
 echo 'Deploying site...'
-scp -r _site/* github@damour.xyz:/var/www/damour.xyz:/var/www/damour.xyz -i sshkey
+scp -r _site/* github@damour.xyz:/var/www/damour.xyz:/var/www/damour.xyz
 
 echo 'Site deployed!'
+
+rm -rf /root/.ssh/
