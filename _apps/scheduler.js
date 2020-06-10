088dd51ff176dd293ab786930f60e28b1eb821ca
damouryouknow <bailey@damour.xyz>
2020-06-09 22:57:14 -0400

    Add time picker

--- _apps/scheduler.js
+++ _apps/scheduler.js
@@ -11,7 +11,53 @@ document.addEventListener('DOMContentLoaded', () => {
         };
         await post('http://localhost:3939/events', data);
     };
+
+    const hourField = $('hour');
+    hourField.onchange = () => {
+        leadingZeroes(hourField, 2);
+        if (Number(hourField.value) < 1) {
+            hourField.value = '12'
+        }
+        else if (Number(hourField.value) > 12) {
+            hourField.value = '01'
+        }
+    };
+
+    const minuteField = $('minute');
+    minuteField.onchange = () => {
+        leadingZeroes(minuteField, 2);
+        if (Number(minuteField.value) < 0) {
+            minuteField.value = '59'
+        }
+        else if (Number(minuteField.value) > 59) {
+            minuteField.value = '00'
+        }
+    };
+
+    hourField.onkeyup = () => {
+        if (hourField.value.length >= 2) {
+            minuteField.focus();
+        }
+    };
+
+    const amBtn = $('am');
+    const pmBtn = $('pm');
+    amBtn.onclick = () => {
+        amBtn.className = 'btn btn-dark';
+        pmBtn.className = 'btn btn-secondary';
+    };
+    pmBtn.onclick = () => {
+        pmBtn.className = 'btn btn-dark';
+        amBtn.className = 'btn btn-secondary';
+    };  
 });
+
+function leadingZeroes(input, zeroes) {
+    const val = input.value;
+    if (val.length < zeroes) {
+        input.value = `${'0'.repeat(zeroes - val.length)}${val}`
+    }
+}
 
 async function post(url, data) {
     return new Promise((resolve, reject) => {
